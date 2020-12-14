9b17c9c491806674696cc5541589584c534e0fa9
Christian Vikkelsø <chri21i5@edu.nextkbh.dk>
2020-12-14 14:28:53 +0100

    Arbejde med Material Design i 141220

---
+++ 091220/test.js
@@ -0,0 +1,6 @@
+var testType = "Hello world";
+console.log(testType);
+function greeting() {
+    return "Surprise";
+}
+console.log(greeting());
---
+++ 091220/test.ts
@@ -0,0 +1,7 @@
+var testType:string = "Hello world"
+console.log(testType)
+
+function greeting() { 
+   return "Surprise";  
+} 
+console.log(greeting())
